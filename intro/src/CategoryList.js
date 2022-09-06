10bf819dadec399817f88e596b14a132349fc7b8
Berke Sayın <sayinberkesayin@gmail.com>
2022-09-06 15:23:02 +0300

    Use States for CategoryList.

--- intro/src/CategoryList.js
+++ intro/src/CategoryList.js
@@ -14,7 +14,12 @@ export default class CategoryList extends Component {
 
   constructor(props){
     super(props);
-    state:{}
+    // this.state = {counter:1}  ==>  Örnek amaçlı bir state
+    this.state = {
+      categories: [
+        {categoryId: 1, categoryName: "Beverages"},
+        {categoryId: 2, categoryName: "Condiments"}
+    ]};
   }
 
   render() {
@@ -22,12 +27,14 @@ export default class CategoryList extends Component {
       <div>
         <h3>{this.props.info.title}</h3>
 
+        <h3>{this.state.counter}</h3>
+
         <ListGroup>
-          <ListGroupItem>Cras justo odio</ListGroupItem>
-          <ListGroupItem>Dapibus ac facilisis in</ListGroupItem>
-          <ListGroupItem>Morbi leo risus</ListGroupItem>
-          <ListGroupItem>Porta ac consectetur ac</ListGroupItem>
-          <ListGroupItem>Vestibulum at eros</ListGroupItem>
+          {
+            this.state.categories.map(category=>(
+              <ListGroupItem key={category.categoryId} >{category.categoryName}</ListGroupItem>
+            ))
+          }          
         </ListGroup>
       </div>
     );
