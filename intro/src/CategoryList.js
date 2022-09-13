31b3749e46adb3929be2ea9ed55be205a40434ac
Berke Sayın <sayinberkesayin@gmail.com>
2022-09-13 13:07:38 +0300

    Use componentDidMount() and Fetch to get data.

--- intro/src/CategoryList.js
+++ intro/src/CategoryList.js
@@ -11,17 +11,20 @@ import React, { Component } from "react";
 import { ListGroup, ListGroupItem } from "reactstrap";
 
 export default class CategoryList extends Component {
-
   // state = {counter:1}  ==>  Örnek amaçlı bir state
   state = {
-    categories: [
-      {categoryId: 1, categoryName: "Beverages"},
-      {categoryId: 2, categoryName: "Condiments"}
-    ],
-    
+    categories: [],
   };
 
-  
+  componentDidMount() {
+    this.getCategories();
+  }
+
+  getCategories = () => {
+    fetch("http://localhost:3000/categories")
+      .then((response) => response.json())
+      .then((data) => this.setState({ categories: data }));
+  };
 
   render() {
     return (
@@ -31,17 +34,17 @@ export default class CategoryList extends Component {
         <h3>{this.state.counter}</h3>
 
         <ListGroup>
-          {
-            this.state.categories.map(category=>(
-              <ListGroupItem onClick={() => this.props.changeCategory(category)} 
-                key={category.categoryId} >{category.categoryName}
-              </ListGroupItem>
-            ))
-          }          
+          {this.state.categories.map((category) => (
+            <ListGroupItem
+              onClick={() => this.props.changeCategory(category)}
+              key={category.id}
+            >
+              {category.categoryName}
+            </ListGroupItem>
+          ))}
         </ListGroup>
 
         <h4>{this.props.currentCategory}</h4>
-
       </div>
     );
   }
